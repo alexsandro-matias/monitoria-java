eb7718340757c9625cf449f17db9388a11ad50ac
alexsandro-matias <matiasalexsandro@gmail.com>
2022-10-07 19:04:22 -0300

    adicionando comentários para turma

--- primeira_aula/src/primeira_aula/Funcionario.java
+++ primeira_aula/src/primeira_aula/Funcionario.java
@@ -4,4 +4,17 @@ public class Funcionario {
 	int matricula;
 	String nome;
 	String escolaridade;
+	double salario;
+
+	public Funcionario(double salario, String nome) {
+		super();
+		this.salario = salario;
+		this.nome = nome;
+	}
+
+	public Funcionario(String nome) {
+		this.nome = nome;
+	}
+
+
 }
--- primeira_aula/src/primeira_aula/Test.java
+++ primeira_aula/src/primeira_aula/Test.java
@@ -2,11 +2,27 @@ package primeira_aula;
 
 public class Test {
 	public static void main(String[] args) {
-		Funcionario alexsandro = new Funcionario();
+
+//		Utilizando um construtor com dois parâmetros
+		Funcionario sandro = new Funcionario(1000, "Alexsandro");
+
+//		Utilizando um construtor com apenas um parâmetro
+		Funcionario segundo = new Funcionario("matias");
+
+//		Utilizando um construtor com apenas um parâmetro
+		Funcionario alexsandro = new Funcionario("alex");
+
+//		Outra forma de atribuir os valores de atributos (hard code)
 		alexsandro.escolaridade = "ensino superior completo";
 
-		System.out.println(alexsandro.escolaridade);
+//		imprimindo o valor de referência da variavel
 		System.out.println(alexsandro);
+		System.out.println(sandro);
+
+//		imprimindo o valor dos atributos de um objeto.
+		System.out.println(sandro.nome);
+		System.out.println(sandro.salario);
+		System.out.println(alexsandro.escolaridade);
 
 	}
 
