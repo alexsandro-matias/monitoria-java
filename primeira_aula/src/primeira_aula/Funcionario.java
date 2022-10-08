5edd936b04b46b8abfcbcc016b14a09b550f38f7
alexsandro-matias <matiasalexsandro@gmail.com>
2022-10-08 18:02:23 -0300

    palavra reservado this

--- primeira_aula/src/primeira_aula/Funcionario.java
+++ primeira_aula/src/primeira_aula/Funcionario.java
@@ -1,20 +1,14 @@
 package primeira_aula;
 
 public class Funcionario {
-	int matricula;
-	String nome;
-	String escolaridade;
-	double salario;
+    int matricula;
+    String nome;
+    String escolaridade;
+    double salario;
 
-	public Funcionario(double salario, String nome) {
-		super();
-		this.salario = salario;
-		this.nome = nome;
-	}
-
-	public Funcionario(String nome) {
-		this.nome = nome;
-	}
-
-
+    public Funcionario(String escolaridade)
+    {
+        this.escolaridade = escolaridade;
+    }
 }
+
--- primeira_aula/src/primeira_aula/Test.java
+++ primeira_aula/src/primeira_aula/Test.java
@@ -1,29 +1,11 @@
 package primeira_aula;
 
 public class Test {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-//		Utilizando um construtor com dois parâmetros
-		Funcionario sandro = new Funcionario(1000, "Alexsandro");
+		Funcionario bill = new Funcionario("Curso superior");
+		System.out.println(bill.escolaridade);
 
-//		Utilizando um construtor com apenas um parâmetro
-		Funcionario segundo = new Funcionario("matias");
-
-//		Utilizando um construtor com apenas um parâmetro
-		Funcionario alexsandro = new Funcionario("alex");
-
-//		Outra forma de atribuir os valores de atributos (hard code)
-		alexsandro.escolaridade = "ensino superior completo";
-
-//		imprimindo o valor de referência da variavel
-		System.out.println(alexsandro);
-		System.out.println(sandro);
-
-//		imprimindo o valor dos atributos de um objeto.
-		System.out.println(sandro.nome);
-		System.out.println(sandro.salario);
-		System.out.println(alexsandro.escolaridade);
-
-	}
+    }
 
 }
