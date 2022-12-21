4dc19fd3852e3f788de6c79ec7411555653909cf
alexsandro-matias <matiasalexsandro@gmail.com>
2022-12-21 20:21:17 -0300

    Refatorando método de impressão de relatório da Turma.

--- primeira_aula/src/colecoes/Main.java
+++ primeira_aula/src/colecoes/Main.java
@@ -1,23 +1,24 @@
 package colecoes;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main
 {
 	public static void main(String[] args)
 	{
-		ArrayList<Aluno> listaAlunos = new ArrayList<>();
-		ArrayList<Professor> listaProfessores = new ArrayList<>();
-		Aluno aluno1 = new Aluno("Alexsandro", "1231231");
-		Aluno aluno2 = new Aluno("João", "4234234");
-		listaAlunos.add(aluno1);
-		listaAlunos.add(aluno2);
-		Professor professor1 = new Professor("Paulo Guedes", "465464");
-		Professor professor2 = new Professor("Anderson Moreira", "9878798");
-		listaProfessores.add(professor1);
-		listaProfessores.add(professor2);
-		Turma oo = new Turma(listaAlunos, listaProfessores);
-		System.out.println(oo);
-		System.out.println(listaProfessores.size());
+		 List<Aluno> listaAlunos = new ArrayList<>();
+		 List<Professor> listaProfessores = new ArrayList<>();
+		 Aluno aluno1 = new Aluno("Alexsandro", "1231231");
+		 Aluno aluno2 = new Aluno("João", "4234234");
+		 listaAlunos.add(aluno1);
+		 listaAlunos.add(aluno2);
+		 Professor professor1 = new Professor("Paulo Guedes", "465464");
+		 Professor professor2 = new Professor("Anderson Moreira", "9878798");
+		 listaProfessores.add(professor1);
+		 listaProfessores.add(professor2);
+		 Turma oo = new Turma(1,"Programação Orientada a Objetos", listaAlunos, listaProfessores);
+		 
+		 System.out.println(oo);
 	}
 }
--- primeira_aula/src/colecoes/Turma.java
+++ primeira_aula/src/colecoes/Turma.java
@@ -1,26 +1,41 @@
 package colecoes;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Turma
 {
-	private ArrayList<Aluno> listaAlunos;
-	private ArrayList<Professor> listaProfessores;
+	private List<Aluno> listaAlunos;
+	private List<Professor> listaProfessores;
+	private String nome;
+	private int codigo;
 	
-	public Turma(ArrayList<Aluno> listaAlunos, ArrayList<Professor> listaProfessores)
+	public Turma(int codigoTurma, String nome, List<Aluno> listaAlunos, List<Professor> listaProfessores)
 	{
 		this.listaAlunos = listaAlunos;
 		this.listaProfessores = listaProfessores;
+		this.nome = nome;
+		this.codigo = codigoTurma;
 	}
 	
-	
-	
-	
-
 	@Override public String toString()
 	{
-		return "Turma [listaAlunos=" + listaAlunos + ", listaProfessores=" + listaProfessores + "]";
+		return "Turma: " + getNome() + " - " + "Código - " + this.codigo + "\n" + listaAlunos + ", listaProfessores="
+				+ listaProfessores + "]";
 	}
 	
+	public String getNome()
+	{
+		return nome;
+	}
 	
+	public void setNome(String nome)
+	{
+		this.nome = nome;
+	}
+	
+	private void impressaoRelatorioTurma()
+	{
+		System.out.println("Turma: " + this.getNome());
+	}
 }
