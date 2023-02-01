775d9cacd7c0ba1994773a128b95c8df8add331b
alexsandro-matias <matiasalexsandro@gmail.com>
2023-01-31 22:06:54 -0300

    Tirando os warnings do projeto

--- primeira_aula/src/escola/Atleta.java
+++ primeira_aula/src/escola/Atleta.java
@@ -3,14 +3,12 @@ package escola;
 public interface Atleta
 {
 	public void treinar();
-	
 	// a sintaxe do método abstrato seria essa aqui.
-//	public void competir();
-
+	// public void competir();
+	
 	// exemplo de um método default.
 	public default void competir()
 	{
 		System.out.println("Atleta competiu.");
 	}
-
 }
