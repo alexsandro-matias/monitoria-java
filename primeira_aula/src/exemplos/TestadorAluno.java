131bdfa6162ae22b462640ac7ad49ef939e8139d
alexsandro-matias <matiasalexsandro@gmail.com>
2023-03-03 10:09:44 -0300

    Enviando classes Proprieties

--- primeira_aula/src/exemplos/TestadorAluno.java
+++ primeira_aula/src/exemplos/TestadorAluno.java
@@ -5,42 +5,33 @@ public class TestadorAluno
 	public static void main(String[] args)
 	{
 		TestadorAluno testador = new TestadorAluno();
-		
 		testador.testarEquals();
-
 	}
-
+	
 	private void testarEquals()
 	{
 		Aluno primeiroAluno, segundoAluno, terceiroAluno;
-		
 		primeiroAluno = new Aluno("João", "20222y6-rc1234");
-		segundoAluno  = new Aluno("João", "20222y6-rc1234");
-		
+		segundoAluno = new Aluno("João", "20222y6-rc1234");
 		terceiroAluno = new Aluno("José", "20222y6-rc1234");
-		
-//		terceiroAluno = new Aluno(null, "20222y6-rc1234");
-		
+		// terceiroAluno = new Aluno(null, "20222y6-rc1234");
 		primeiroAluno = new Aluno(null, null);
-		
 		Aluno aluno = new Aluno("Alexsandro", "15644564561465");
-		
 		comparaAlunos(primeiroAluno, primeiroAluno);
-//		comparaAlunos(primeiroAluno, segundoAluno);
-//		comparaAlunos(terceiroAluno, segundoAluno);
-//		comparaAlunos(primeiroAluno, terceiroAluno);
-//		
-//		String meuTexto = "bom dia!"; 
-//		comparaAlunos(primeiroAluno, meuTexto);
-		
-//		comparaAlunos(primeiroAluno, null);
+		// comparaAlunos(primeiroAluno, segundoAluno);
+		// comparaAlunos(terceiroAluno, segundoAluno);
+		// comparaAlunos(primeiroAluno, terceiroAluno);
+		//
+		// String meuTexto = "bom dia!";
+		// comparaAlunos(primeiroAluno, meuTexto);
+		// comparaAlunos(primeiroAluno, null);
 		comparaAlunos(null, primeiroAluno);
 	}
-
-	//private void comparaAlunos(Aluno primeiroAluno, Aluno segundoAluno)
+	
+	// private void comparaAlunos(Aluno primeiroAluno, Aluno segundoAluno)
 	private void comparaAlunos(Object primeiroAluno, Object segundoAluno)
 	{
-		if(primeiroAluno.equals(segundoAluno))
+		if (primeiroAluno.equals(segundoAluno))
 		{
 			System.out.println("Alunos têm mesmo conteúdo.");
 		}
@@ -52,22 +43,21 @@ public class TestadorAluno
 	
 	private boolean testeArrayList(Object elemento)
 	{
-		// Returns true if this list contains the specified 
-		// element. More formally, returns true if and only if 
+		// Returns true if this list contains the specified
+		// element. More formally, returns true if and only if
 		// this list contains at least one element e
-		//  such that 
+		// such that
 		// (o==null ? e==null : o.equals(e)).
-		if(elemento == null)
+		if (elemento == null)
 		{
 			// percorre a lista procurando um elemento null
-			// usando o operador 	'
+			// usando o operador '
 		}
 		else
 		{
-			// percorre a lista procurando um elemento 
+			// percorre a lista procurando um elemento
 			// que o método equals retorna true
 		}
 		return false;
 	}
-
 }
